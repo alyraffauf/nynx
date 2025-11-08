ef0f232b935aae8a8b0bda0693510693ee2e5bbf
Aly Raffauf <aly@aly.codes>
2025-11-07 19:23:54 -0500

    flake: add wrapped version with lix-eval-jobs

--- flake/packages.nix
+++ flake/packages.nix
@@ -25,6 +25,26 @@ _: {
           else "dev";
       };
 
+      nynx-lix = pkgs.buildGoModule {
+        pname = "nynx";
+        src = ../src;
+        vendorHash = null;
+
+        nativeBuildInputs = with pkgs; [
+          makeWrapper
+        ];
+
+        postInstall = ''
+          wrapProgram $out/bin/nynx \
+            --prefix PATH : ${pkgs.lib.makeBinPath [pkgs.lixPackageSets.latest.nix-eval-jobs]}
+        '';
+
+        version =
+          if self' ? shortRev
+          then "git-${self'.shortRev}"
+          else "dev";
+      };
+
       default = nynx;
     };
   };
